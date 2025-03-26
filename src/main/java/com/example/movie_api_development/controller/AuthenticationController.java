da7e9750457284c8376cd10400150a8d7ec05c36
sabiaydin <sabinamammadova871@gmail.com>
2025-03-26 21:20:04 +0400

    some changes

--- src/main/java/com/example/movie_api_development/controller/AuthenticationController.java
+++ src/main/java/com/example/movie_api_development/controller/AuthenticationController.java
@@ -4,13 +4,12 @@ import com.example.movie_api_development.model.dto.request.AuthenticationRequest
 import com.example.movie_api_development.model.dto.request.RegisterRequest;
 import com.example.movie_api_development.model.dto.response.AuthenticationResponse;
 import com.example.movie_api_development.service.AuthenticationService;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
--- src/main/java/com/example/movie_api_development/controller/MovieController.java
+++ src/main/java/com/example/movie_api_development/controller/MovieController.java
@@ -12,7 +12,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/movies")
 @RequiredArgsConstructor
